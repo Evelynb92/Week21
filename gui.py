91f5d33114077bd86f21a9c06e33314c635469f4
ebrooks <evelynb92@hotmail.com>
2022-07-20 14:20:42 -0400

    Added image

--- gui.py
+++ gui.py
@@ -1,7 +1,5 @@
 import tkinter as tk
-from tkinter import *
-from tkinter import ttk
-from tkinter import PhotoImage
+import ttk
 
 # Window Box -------------------------
 
@@ -11,20 +9,20 @@ root.title('Contact Book')
 # Add Tab -------------------------
 
 tabControl = ttk.Notebook(root, width=570, height=350)
-add_tab = Frame(tabControl)
-search_tab = Frame(tabControl)
+add_tab = tk.Frame(tabControl)
+search_tab = tk.Frame(tabControl)
 tabControl.add(add_tab, text='Add Contacts')
 tabControl.add(search_tab, text='Search Contacts')
 tabControl.grid(row=0, column=0)
 
 # Background Images -------------------------
 
-bg_img = PhotoImage(file='contactbook.png')
-background_label_add = Label(add_tab, image=bg_img)
-background_label_search = Label(search_tab, image=bg_img)
+bg_img = tk.PhotoImage(file='contactbook.png')
+background_label_add = tk.Label(add_tab, image=bg_img)
+background_label_search = tk.Label(search_tab, image=bg_img)
 
 # Buttons -------------------------
 
-add_button = Button(add_tab, text='Add Contact')
+add_button = tk.Button(add_tab, text='Add Contact')
 
-search_button = Button(search_tab, text='Search Contact')
+search_button = tk.Button(search_tab, text='Search Contact')
